0c4c255a41da4d5d3587c945ff7159b2f951c3d3
velvet-halation <buizark@gmail.com>
2019-09-26 06:37:08 -0700

    Strike

--- src/main/java/Piruru/cards/PiruruCard.java
+++ src/main/java/Piruru/cards/PiruruCard.java
@@ -3,16 +3,23 @@ package Piruru.cards;
 import Piruru.Piruru;
 import Piruru.characters.PiruruChar;
 import basemod.abstracts.CustomCard;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 
 public abstract class PiruruCard extends CustomCard {
+    protected CardStrings cardStrings;
 
-    public PiruruCard(int cost, String rawDescription, CardType type, CardRarity rarity, CardTarget target) {
-        super(null, null, (String)null, cost, rawDescription, type, PiruruChar.Enums.PIRURU_ICE, rarity, target);
+
+    public PiruruCard(int cost, CardType type, CardRarity rarity, CardTarget target) {
+        super(null, null, (String)null, cost, null, type, PiruruChar.Enums.PIRURU_ICE, rarity, target);
         cardID = this.getClass().getSimpleName();
-        CardStrings cardStrings =  CardCrawlGame.languagePack.getCardStrings(cardID);;
+        cardStrings =  CardCrawlGame.languagePack.getCardStrings(cardID);;
         name = cardStrings.NAME;
+        rawDescription = cardStrings.DESCRIPTION;
     }
 
     private static String getImg(String id) {
@@ -24,4 +31,11 @@ public abstract class PiruruCard extends CustomCard {
         return Piruru.makeID(this.getClass().getSimpleName());
     }
 
+    protected void act(AbstractGameAction a) {
+        AbstractDungeon.actionManager.addToBottom(a);
+    }
+
+    protected DamageInfo dmgInfo() {
+        return new DamageInfo(AbstractDungeon.player, damage, DamageInfo.DamageType.NORMAL);
+    }
 }
--- src/main/java/Piruru/cards/Strike.java
+++ src/main/java/Piruru/cards/Strike.java
@@ -1,7 +1,31 @@
 package Piruru.cards;
 
 import Piruru.Piruru;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class Strike extends PiruruCard {
-    public static final String id = Piruru.makeID(Strike.class.getSimpleName());
+    private static final int COST = 1;
+    private static final CardType TYPE = CardType.ATTACK;
+    private static final CardRarity RARITY = CardRarity.BASIC;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final int DAMAGE = 6;
+    private static final int DAMAGE_UP = 3;
+
+    public Strike() {
+        super(COST, TYPE, RARITY, TARGET);
+        baseDamage = damage = DAMAGE;
+    }
+
+    @Override
+    public void upgrade() {
+        upgradeDamage(DAMAGE_UP);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        act(new DamageAction(m, dmgInfo()));
+    }
 }
