f8dbceefe5ccc309ea48a5ad55b2d808e8333840
velvet-halation <buizark@gmail.com>
2019-09-26 05:33:41 -0700

    texture loader and idc anymore dude just go

---
+++ src/main/java/Piruru/relics/PiruruRelic.java
@@ -0,0 +1,12 @@
+package Piruru.relics;
+
+import basemod.abstracts.CustomRelic;
+import com.badlogic.gdx.graphics.Texture;
+
+public class PiruruRelic extends CustomRelic {
+
+    public PiruruRelic(String id, Texture texture, RelicTier tier, LandingSound sfx) {
+        super(id, texture, tier, sfx);
+    }
+
+}
---
+++ src/main/java/Piruru/relics/StarterRelic.java
@@ -0,0 +1,15 @@
+package Piruru.relics;
+
+import Piruru.Piruru;
+import com.badlogic.gdx.graphics.Texture;
+
+public class StarterRelic extends PiruruRelic {
+    public static final String ID = Piruru.makeID(StarterRelic.class.getSimpleName());
+    public static final Texture =
+
+
+    public StarterRelic() {
+        super(ID, TEXTURE, TIER, SFX);
+    }
+
+}
