e5ca66ef5303f169499d8fde3fb5431c6f1cdbda
velvet-halation <buizark@gmail.com>
2019-09-26 06:25:13 -0700

    never write a name or id ever again

--- src/main/java/Piruru/cards/PiruruCard.java
+++ src/main/java/Piruru/cards/PiruruCard.java
@@ -7,18 +7,21 @@ import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 
 public abstract class PiruruCard extends CustomCard {
-    public String id;
 
-    public PiruruCard(String id, int cost, String rawDescription, CardType type, CardRarity rarity, CardTarget target) {
-        super(id, null, getImg(id), cost, rawDescription, type, PiruruChar.Enums.PIRURU_ICE, rarity, target);
-        this.id = id;
-        CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(id);
+    public PiruruCard(int cost, String rawDescription, CardType type, CardRarity rarity, CardTarget target) {
+        super(null, null, (String)null, cost, rawDescription, type, PiruruChar.Enums.PIRURU_ICE, rarity, target);
+        cardID = this.getClass().getSimpleName();
+        CardStrings cardStrings =  CardCrawlGame.languagePack.getCardStrings(cardID);;
         name = cardStrings.NAME;
     }
 
     private static String getImg(String id) {
         String imgName = id.substring((id.indexOf(":") + 1)).trim();
         return Piruru.makeCardPath(imgName);
+    }
+
+    protected String makeName() {
+        return Piruru.makeID(this.getClass().getSimpleName());
     }
 
 }
---
+++ src/main/java/Piruru/cards/Strike.java
@@ -0,0 +1,7 @@
+package Piruru.cards;
+
+import Piruru.Piruru;
+
+public class Strike extends PiruruCard {
+    public static final String id = Piruru.makeID(Strike.class.getSimpleName());
+}
