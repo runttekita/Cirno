14c4a1e2dec273282c0d13b7ef5d72cd24a1855f
velvet-halation <buizark@gmail.com>
2019-09-26 05:39:01 -0700

    Texture stuff

--- src/main/java/Piruru/relics/PiruruRelic.java
+++ src/main/java/Piruru/relics/PiruruRelic.java
@@ -1,12 +1,19 @@
 package Piruru.relics;
 
+import Piruru.TextureLoader;
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
 
 public class PiruruRelic extends CustomRelic {
 
-    public PiruruRelic(String id, Texture texture, RelicTier tier, LandingSound sfx) {
-        super(id, texture, tier, sfx);
+    public PiruruRelic(String id, RelicTier tier, LandingSound sfx) {
+        super(id, getTexture(id), tier, sfx);
+    }
+
+    private static Texture getTexture(String id) {
+        String imgName = id.substring((id.indexOf(":"+1)));
+        imgName.trim();
+        return TextureLoader.getTexture(imgName);
     }
 
 }
--- src/main/java/Piruru/relics/StarterRelic.java
+++ src/main/java/Piruru/relics/StarterRelic.java
@@ -2,14 +2,16 @@ package Piruru.relics;
 
 import Piruru.Piruru;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 public class StarterRelic extends PiruruRelic {
     public static final String ID = Piruru.makeID(StarterRelic.class.getSimpleName());
-    public static final Texture =
+    private static final RelicTier TIER = RelicTier.STARTER;
+    public static final LandingSound SFX = LandingSound.FLAT;
 
 
     public StarterRelic() {
-        super(ID, TEXTURE, TIER, SFX);
+        super(ID, TIER, SFX);
     }
 
 }
