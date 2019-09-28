d3fce525d788a877d00193845e8b92ead162f909
velvet-halation <buizark@gmail.com>
2019-09-28 16:34:25 -0700

    stance change

---
+++ src/main/java/Piruru/actions/NotShittyChangeStanceAction.java
@@ -0,0 +1,47 @@
+package Piruru.actions;
+
+import Piruru.stances.PiruruStance;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.watcher.CannotChangeStancePower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.stances.AbstractStance;
+
+public class NotShittyChangeStanceAction extends AbstractGameAction {
+    private AbstractStance newStance;
+
+    public NotShittyChangeStanceAction(AbstractStance newStance) {
+        this.newStance = newStance;
+    }
+
+    @Override
+    public void update() {
+        if (AbstractDungeon.player.hasPower(CannotChangeStancePower.POWER_ID)) {
+            isDone = true;
+            return;
+        }
+
+        AbstractStance oldStance = AbstractDungeon.player.stance;
+        if (!oldStance.ID.equals(newStance.ID)) {
+            for (AbstractPower p : AbstractDungeon.player.powers) {
+                p.onChangeStance(oldStance, newStance);
+            }
+            for (AbstractRelic r : AbstractDungeon.player.relics) {
+                r.onChangeStance(oldStance, newStance);
+            }
+            oldStance.onExitStance();
+            AbstractDungeon.player.stance = newStance;
+            newStance.onEnterStance();
+            AbstractDungeon.actionManager.stancesSwitchedThisTurn.add(newStance);
+            AbstractDungeon.player.switchedStance();
+            for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
+                c.triggerExhaustedCardsOnStanceChange(newStance);
+            }
+           AbstractDungeon.player.onStanceChange(newStance.ID);
+        }
+        AbstractDungeon.onModifyPower();
+        isDone = true;
+    }
+}
---
+++ src/main/java/Piruru/stances/PiruruStance.java
@@ -0,0 +1,10 @@
+package Piruru.stances;
+
+import com.megacrit.cardcrawl.stances.AbstractStance;
+
+public class PiruruStance extends AbstractStance {
+    @Override
+    public void updateDescription() {
+
+    }
+}
