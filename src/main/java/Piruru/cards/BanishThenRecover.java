5f44d4690903a1bcc22e75d4ed546d3a9e44aefb
velvet-halation <buizark@gmail.com>
2019-09-30 02:36:47 -0700

    slgiht rework on this one

--- src/main/java/Piruru/cards/BanishThenRecover.java
+++ src/main/java/Piruru/cards/BanishThenRecover.java
@@ -13,30 +13,20 @@ import static Piruru.Piruru.makeID;
 public class BanishThenRecover extends PiruruCard {
     private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(
             makeID(BanishThenRecover.class.getSimpleName()));
-    private static final int COST = 1;
+    private static final int COST = 2;
     private static final CardType TYPE = CardType.SKILL;
     private static final CardRarity RARITY = CardRarity.UNCOMMON;
     private static final CardTarget TARGET = CardTarget.SELF;
-    private static final int RECOVER_AMT = 1;
-    private static final int RECOVER_UP = 1;
+    private static final int RECOVER_AMT = 2;
+    private static final int COST_UP = 1;
 
     public BanishThenRecover() {
-        super(cardStrings, COST, TYPE, RARITY, TARGET, 0, 0, RECOVER_UP, COST);
+        super(cardStrings, COST, TYPE, RARITY, TARGET, 0, 0, 0, COST_UP);
         baseMagicNumber = magicNumber = RECOVER_AMT;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         act(new BanishAction(magicNumber, ifSuccess -> act(new RecoverAction(magicNumber))));
-    }
-
-    @Override
-    public void upgrade() {
-        if (!upgraded) {
-            upgradeName();
-            upgradeMagicNumber(RECOVER_UP);
-            rawDescription = cardStrings.UPGRADE_DESCRIPTION;
-            initializeDescription();
-        }
     }
 }
