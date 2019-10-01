868c6f615320254c15942e334325aea7924ffd04
velvet-halation <buizark@gmail.com>
2019-10-01 05:36:17 -0700

    give interface function

---
+++ src/main/java/Piruru/patches/OnRefreshHandPatch.java
@@ -0,0 +1,34 @@
+package Piruru.patches;
+
+import Piruru.OnRefreshHand;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.megacrit.cardcrawl.cards.CardGroup;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import javassist.CtBehavior;
+
+@SpirePatch(
+        clz = CardGroup.class,
+        method = "refreshHandLayout"
+)
+public class OnRefreshHandPatch {
+    @SpireInsertPatch(
+            locator = Locator.class
+    )
+    public static void Insert(CardGroup __instance) {
+        for (AbstractPower p : AbstractDungeon.player.powers) {
+            if (p instanceof OnRefreshHand) {
+                ((OnRefreshHand)p).onRefreshHand();
+            }
+        }
+    }
+
+    public static class Locator extends SpireInsertLocator {
+
+        @Override
+        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
+            Matcher finalMatcher = new Matcher.FieldAccessMatcher(CardGroup.class, "group");
+            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
+        }
+    }
+}
