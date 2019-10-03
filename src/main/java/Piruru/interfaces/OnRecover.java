daeb589cce2cde3e11f898e173cfc6b9f732543b
velvet-halation <buizark@gmail.com>
2019-10-03 00:23:43 -0700

    onrecoverpatch

--- src/main/java/Piruru/interfaces/OnRecover.java
+++ src/main/java/Piruru/interfaces/OnRecover.java
@@ -14,33 +14,4 @@ public interface OnRecover {
 
     public void onRecover(AbstractCard c);
 
-    @SpirePatch(
-            clz = BetterDiscardPileToHandAction.class,
-            method = "update"
-    )
-    public static class HologramTriggerOnRecover {
-        @SpireInsertPatch(
-                locator = Locator.class,
-                localvars = {
-                        "c"
-                }
-        )
-        public static void Insert(BetterDiscardPileToHandAction __instance, AbstractCard c) {
-            for (AbstractPower p : AbstractDungeon.player.powers) {
-                if (p instanceof OnRecover) {
-                    ((OnRecover)p).onRecover(c);
-                }
-            }
-        }
-
-        public static class Locator extends SpireInsertLocator {
-
-            @Override
-            public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
-                Matcher finalMatcher = new Matcher.MethodCallMatcher(CardGroup.class, "addToHand");
-                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
-            }
-        }
-    }
-
 }
