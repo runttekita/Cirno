7e52ca81fc61809567db59f2b7e5060473811dd2
velvet-halation <buizark@gmail.com>
2019-10-03 02:39:33 -0700

    ok kio

--- src/main/java/Piruru/cards/BanishExhume.kt
+++ src/main/java/Piruru/cards/BanishExhume.kt
@@ -22,7 +22,7 @@ class BanishExhume : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_
     }
 
     override fun use(p: AbstractPlayer, m: AbstractMonster?) {
-        act(BanishAction(magicNumber) { ifSuccess -> act(ExhumeButWithOneLessParameter()) })
+        act(BanishAction(magicNumber) { _ -> act(ExhumeButWithOneLessParameter()) })
     }
 
     companion object {
