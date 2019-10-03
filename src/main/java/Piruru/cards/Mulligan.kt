4dfe31e9ece5cb469358e5c9eecb12b42d368e56
velvet-halation <buizark@gmail.com>
2019-10-02 23:33:26 -0700

    mulligan

---
+++ src/main/java/Piruru/cards/Mulligan.kt
@@ -0,0 +1,43 @@
+package Piruru.cards
+
+import Piruru.Piruluk.Statics.makeID
+import Piruru.abstracts.PiruruCard
+import Piruru.actions.DiscardAnyAmountAction
+import Piruru.actions.MulliganAction
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class Mulligan : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MAGIC_UP, COST_UP) {
+    init {
+        damage = DAMAGE
+        baseDamage = damage
+        block = BLOCK
+        baseBlock = block
+        magicNumber = MAGIC
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        act(DiscardAnyAmountAction { list -> act(MulliganAction(list.size)) })
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(Mulligan::class.java.simpleName))
+        private val COST = 1
+        private val TYPE = AbstractCard.CardType.SKILL
+        private val RARITY = AbstractCard.CardRarity.UNCOMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 0
+        private val MAGIC_UP = 0
+        private val DAMAGE = 0
+        private val BLOCK = 0
+        private val MAGIC = 0
+        private val COST_UP = 0
+    }
+}
