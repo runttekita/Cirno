5fbdd39c14cac388082893fb06345b9d64b45a2d
velvet-halation <buizark@gmail.com>
2019-10-03 00:26:01 -0700

    notshittytookdamage

--- src/main/java/Piruru/interfaces/NotShittyTookDamage.java
+++ src/main/java/Piruru/interfaces/NotShittyTookDamage.java
@@ -12,29 +12,4 @@ public interface NotShittyTookDamage {
 
     public void notShittyTookDamage(DamageInfo i);
 
-    @SpirePatch(
-            clz = AbstractPlayer.class,
-            method = "damage"
-    )
-    public static class NotShittyTookDamagePatch {
-        @SpireInsertPatch(
-                locator = Locator.class
-        )
-        public static void Insert(AbstractPlayer __instance, DamageInfo i) {
-            for (AbstractCard c : AbstractDungeon.player.hand.group) {
-                if (c instanceof NotShittyTookDamage) {
-                    ((NotShittyTookDamage)c).notShittyTookDamage(i);
-                }
-            }
-        }
-    }
-
-    public static class Locator extends SpireInsertLocator {
-
-        @Override
-        public int[] Locate(CtBehavior ctMethodToPatch) throws Exception {
-            Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractPlayer.class, "updateCardsOnDamage");
-            return LineFinder.findInOrder(ctMethodToPatch, finalMatcher);
-        }
-    }
 }
