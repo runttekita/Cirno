626fb72d9d10ea81d04daa8b14066fb9118f63bb
velvet-halation <buizark@gmail.com>
2019-10-02 23:27:57 -0700

    attackbanishattack

---
+++ src/main/java/Piruru/cards/AttackBanishAttack.kt
@@ -0,0 +1,45 @@
+package Piruru.cards
+
+import Piruru.Piruluk.Statics.makeID
+import Piruru.abstracts.PiruruCard
+import Piruru.actions.BanishAction
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class AttackBanishAttack : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, BANISH_UP, COST) {
+    init {
+        damage = DAMAGE
+        baseDamage = damage
+        block = BLOCK
+        baseBlock = block
+        magicNumber = BANISH
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        act(BanishAction(
+                magicNumber,
+                { c -> c.type == AbstractCard.CardType.ATTACK },
+                { c -> damage(m) }
+        ))
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(AttackBanishAttack::class.java.simpleName))
+        private val COST = 1
+        private val TYPE = AbstractCard.CardType.ATTACK
+        private val RARITY = AbstractCard.CardRarity.COMMON
+        private val TARGET = AbstractCard.CardTarget.ENEMY
+        private val DAMAGE_UP = 3
+        private val BLOCK_UP = 0
+        private val BANISH_UP = 0
+        private val DAMAGE = 12
+        private val BLOCK = 0
+        private val BANISH = 2
+    }
+}
