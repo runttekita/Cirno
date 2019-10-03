8edfd9738f80365e5de943add3ffac6442011b97
velvet-halation <buizark@gmail.com>
2019-10-02 19:46:28 -0700

    give banishaction a predicate option

--- src/main/java/Piruru/actions/BanishAction.java
+++ src/main/java/Piruru/actions/BanishAction.java
@@ -3,6 +3,7 @@ package Piruru.actions;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -11,14 +12,16 @@ import com.megacrit.cardcrawl.vfx.SpeechBubble;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import static Piruru.Piruru.makeID;
 
 public class BanishAction extends AbstractGameAction {
     private static UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(makeID(BanishAction.class));
     private int amount;
-    private Consumer<ArrayList<AbstractCard>> callback;
+    private Consumer<ArrayList<AbstractCard>> callback = null;
     private boolean anyAmount = false;
+    private Predicate<AbstractCard> predicate = null;
 
     public BanishAction(int amount, Consumer<ArrayList<AbstractCard>> callback) {
         this(amount);
@@ -35,6 +38,12 @@ public class BanishAction extends AbstractGameAction {
         this.anyAmount = anyAmount;
     }
 
+    public BanishAction(int amount, Consumer<ArrayList<AbstractCard>> callback, Predicate<AbstractCard> predicate) {
+        this(amount);
+        this.callback = callback;
+        this.predicate = predicate;
+    }
+
     @Override
     public void update() {
         if (duration == Settings.ACTION_DUR_FASTER) {
@@ -48,6 +57,25 @@ public class BanishAction extends AbstractGameAction {
                 isDone = true;
                 return;
             }
+
+            if (predicate != null) {
+                CardGroup tmp = new CardGroup(CardGroup.CardGroupType.UNSPECIFIED);
+                for (AbstractCard c : AbstractDungeon.player.discardPile.group) {
+                    if (predicate.test(c)) {
+                        tmp.addToBottom(c);
+                    }
+                }
+                if (amount < tmp.size()) {
+                    AbstractDungeon.effectList.add(new SpeechBubble(AbstractDungeon.player.dialogX, AbstractDungeon.player.dialogY, 2.5f,
+                            uiStrings.TEXT[0], true));
+                    isDone = true;
+                    return;
+                }
+                AbstractDungeon.gridSelectScreen.open(tmp, amount, "", false);
+                tickDuration();
+                return;
+            }
+
             if (anyAmount) {
                 AbstractDungeon.gridSelectScreen.open(AbstractDungeon.player.discardPile, amount, true, "");
                 tickDuration();
