81ff90730914ddcf8fc52b31ff441cbed6bbdd38
velvet-halation <buizark@gmail.com>
2019-10-02 23:32:38 -0700

    enter remember

---
+++ src/main/java/Piruru/cards/EnterRemember.kt
@@ -0,0 +1,42 @@
+package Piruru.cards
+
+import Piruru.Piruluk.Statics.makeID
+import Piruru.abstracts.PiruruCard
+import Piruru.actions.NotShittyChangeStanceAction
+import Piruru.stances.Remember
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class EnterRemember : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MAGIC_UP, COST) {
+    init {
+        damage = DAMAGE
+        baseDamage = damage
+        block = BLOCK
+        baseBlock = block
+        magicNumber = MAGIC
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        act(NotShittyChangeStanceAction(Remember()))
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(EnterRemember::class.java.simpleName))
+        private val COST = 2
+        private val TYPE = AbstractCard.CardType.POWER
+        private val RARITY = AbstractCard.CardRarity.UNCOMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 0
+        private val MAGIC_UP = 0
+        private val DAMAGE = 0
+        private val BLOCK = 0
+        private val MAGIC = 0
+    }
+}
