4e8bb1be448a69591d63483bbd93509d1b016694
velvet-halation <buizark@gmail.com>
2019-10-02 23:31:53 -0700

    discardrandomcold

---
+++ src/main/java/Piruru/cards/DiscardRandomCold.kt
@@ -0,0 +1,50 @@
+package Piruru.cards
+
+import Piruru.Piruluk.Statics.makeID
+import Piruru.abstracts.PiruruCard
+import Piruru.actions.DiscardAnyAmountAction
+import Piruru.powers.Cold
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class DiscardRandomCold : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MAGIC_UP, COST_UP) {
+    init {
+        damage = DAMAGE
+        baseDamage = damage
+        block = BLOCK
+        baseBlock = block
+        magicNumber = MAGIC
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        act(DiscardAnyAmountAction { list ->
+            list.forEach { c ->
+                val target = AbstractDungeon.getCurrRoom().monsters.randomMonster
+                act(ApplyPowerAction(target, p, Cold(m), 1))
+            }
+        })
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(DiscardRandomCold::class.java.simpleName))
+        private val COST = 3
+        private val TYPE = AbstractCard.CardType.SKILL
+        private val RARITY = AbstractCard.CardRarity.UNCOMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 0
+        private val MAGIC_UP = 0
+        private val DAMAGE = 0
+        private val BLOCK = 0
+        private val MAGIC = 0
+        private val COST_UP = 2
+    }
+}
