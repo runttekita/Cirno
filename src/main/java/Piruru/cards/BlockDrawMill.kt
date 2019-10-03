4209e25991e20d0b52350958cc82d53efd787395
velvet-halation <buizark@gmail.com>
2019-10-02 23:29:37 -0700

    blockdrawmill

---
+++ src/main/java/Piruru/cards/BlockDrawMill.kt
@@ -0,0 +1,44 @@
+package Piruru.cards
+
+import Piruru.Piruluk.Statics.makeID
+import Piruru.abstracts.PiruruCard
+import Piruru.actions.MillAction
+import com.megacrit.cardcrawl.actions.common.DrawCardAction
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class BlockDrawMill : PiruruCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MILL_DRAW_UP, COST) {
+    init {
+        damage = DAMAGE
+        baseDamage = damage
+        block = BLOCK
+        baseBlock = block
+        magicNumber = MILL_DRAW
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster) {
+        block()
+        act(DrawCardAction(magicNumber))
+        act(MillAction(magicNumber))
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(BlockDrawMill::class.java.simpleName))
+        private val COST = 1
+        private val TYPE = AbstractCard.CardType.SKILL
+        private val RARITY = AbstractCard.CardRarity.COMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 3
+        private val MILL_DRAW_UP = 1
+        private val DAMAGE = 0
+        private val BLOCK = 8
+        private val MILL_DRAW = 1
+    }
+}
