d7a8aeb7e0b25ea334da0140cc1a0a96761bd813
velvet-halation <buizark@gmail.com>
2019-10-05 04:05:41 -0700

    YUI????

--- src/main/kotlin/Piruru/ui/ARTSZone.kt
+++ src/main/kotlin/Piruru/ui/ARTSZone.kt
@@ -3,7 +3,7 @@ package Piruru.ui
 import Piruru.Piruluk
 import reina.yui.YuiClickableObject;
 
-class ARTSZone : YuiClickableObject(Piruluk.textureLoader.getTexture("Piruru/images/ui/ARTS.jpg"), 2f, 250f) {
+class ARTSZone : YuiClickableObject(Piruluk.textureLoader.getTexture("Piruru/images/ui/ARTS.jpg"), 1.32f, 165f) {
 
     override fun onClick() {
 
