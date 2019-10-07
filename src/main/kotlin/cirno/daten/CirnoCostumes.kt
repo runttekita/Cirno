d5ed30abf68117357a51becfcd75d1f08149bb4a
velvet-halation <buizark@gmail.com>
2019-10-07 09:32:03 -0700

    custom savable for skins

--- src/main/kotlin/cirno/daten/CirnoCostumes.kt
+++ src/main/kotlin/cirno/daten/CirnoCostumes.kt
@@ -14,16 +14,21 @@ import reina.yui.Yui
 import reina.yui.YuiClickableObject
 import com.megacrit.cardcrawl.screens.charSelect.CharacterSelectScreen
 import basemod.ReflectionHacks
+import basemod.abstracts.CustomSavable
 import com.megacrit.cardcrawl.core.Settings
 import com.megacrit.cardcrawl.helpers.FontHelper
 import java.util.ArrayList
 
 
 
-class CirnoCostumes: YuiClickableObject(textureLoader.getTexture(""), -500f, -500f) {
+class CirnoCostumes: YuiClickableObject(textureLoader.getTexture(""), -500f, -500f), CustomSavable<Int> {
 
     var currentCostume = 1
     private var cirnoButton: CharacterOption? = null
+
+    init {
+        BaseMod.addSaveField<Int>("currentCostume", this)
+    }
 
     override fun render(sb: SpriteBatch) {
         if (CardCrawlGame.mode == CardCrawlGame.GameMode.CHAR_SELECT && CardCrawlGame.chosenCharacter == CirnoChar.Enums.enums.Cirno) {
@@ -42,6 +47,14 @@ class CirnoCostumes: YuiClickableObject(textureLoader.getTexture(""), -500f, -50
                 FontHelper.renderFontCentered(sb, FontHelper.energyNumFontBlue, "Click the character button\n to change skins.", x + image.width * Settings.scale, y)
             }
         }
+    }
+
+    override fun onLoad(costume: Int) {
+        currentCostume = costume
+    }
+
+    override fun onSave(): Int {
+        return currentCostume
     }
 
     public fun getCurrentCostume(): Texture {
