36ea07c4302496742c6d7d032bad98dcdc373d13
velvet-halation <buizark@gmail.com>
2019-10-08 06:28:18 -0700

    exordium only

--- src/main/kotlin/cirno/daten/Events.kt
+++ src/main/kotlin/cirno/daten/Events.kt
@@ -2,11 +2,12 @@ package cirno.daten
 
 import basemod.BaseMod
 import cirno.events.AMeetingWithAFriend
+import com.megacrit.cardcrawl.dungeons.Exordium
 
 class Events {
 
     init {
-        BaseMod.addEvent(AMeetingWithAFriend.ID, AMeetingWithAFriend::class.java)
+        BaseMod.addEvent(AMeetingWithAFriend.ID, AMeetingWithAFriend::class.java, Exordium.ID)
     }
 
 }
