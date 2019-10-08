dab8b21f4f798e37f87ac6a1da2e61a110d35d4d
velvet-halation <buizark@gmail.com>
2019-10-08 06:34:28 -0700

    cirno only

--- src/main/kotlin/cirno/daten/Events.kt
+++ src/main/kotlin/cirno/daten/Events.kt
@@ -1,13 +1,31 @@
 package cirno.daten
 
 import basemod.BaseMod
+import cirno.characters.CirnoChar
 import cirno.events.AMeetingWithAFriend
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.dungeons.Exordium
 
 class Events {
 
     init {
         BaseMod.addEvent(AMeetingWithAFriend.ID, AMeetingWithAFriend::class.java, Exordium.ID)
+    }
+
+    @SpirePatch (
+        clz = AbstractDungeon::class,
+        method = "initializeCardPools"
+    )
+    public class CirnoONLY {
+        public companion object {
+            @JvmStatic
+            fun Prefix(__instance: AbstractDungeon) {
+                if (AbstractDungeon.player !is CirnoChar) {
+                    AbstractDungeon.eventList.remove(AMeetingWithAFriend.ID)
+                }
+            }
+        }
     }
 
 }
