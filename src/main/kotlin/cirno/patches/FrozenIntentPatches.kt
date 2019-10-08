2fa0241d47c295dfccaa76b874a968da36c3f805
velvet-halation <buizark@gmail.com>
2019-10-07 23:13:17 -0700

    blue

--- src/main/kotlin/cirno/patches/FrozenIntentPatches.kt
+++ src/main/kotlin/cirno/patches/FrozenIntentPatches.kt
@@ -2,11 +2,30 @@ package cirno.patches
 
 import cirno.Cirno
 import basemod.ReflectionHacks
+import cirno.Cirno.Statics.makeID
 import com.badlogic.gdx.graphics.Texture
 import com.evacipated.cardcrawl.modthespire.lib.*
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.helpers.PowerTip
 import com.megacrit.cardcrawl.monsters.AbstractMonster
+import com.evacipated.cardcrawl.modthespire.lib.LineFinder
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch
+import com.evacipated.cardcrawl.modthespire.lib.Matcher.MethodCallMatcher
+import javassist.CtBehavior
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertLocator
+import com.esotericsoftware.spine.SkeletonMeshRenderer
+import java.util.Arrays
+import com.evacipated.cardcrawl.modthespire.lib.Matcher.FieldAccessMatcher
+import com.megacrit.cardcrawl.monsters.exordium.Hexaghost
+import cirno.powers.Frozen
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+
+
 
 object FrozenIntentPatches {
 
@@ -49,6 +68,91 @@ object FrozenIntentPatches {
                 return SpireReturn.Return<Any>(null)
             }
             return SpireReturn.Continue<Any>()
+        }
+    }
+
+    @SpirePatch(clz = AbstractMonster::class, method = "render")
+    object FrozenIntentShading {
+        private val shader = ShaderProgram(
+                Gdx.files.internal("Piruru/shaders/frozen/vertexShader.vs"),
+                Gdx.files.internal("Piruru/shaders/frozen/fragShader.fs")
+        )
+
+
+        @SpireInsertPatch(locator = LocatorImageStart::class, localvars = ["atlas"])
+        @JvmStatic
+        fun InsertImageStart(__instance: AbstractMonster, sb: SpriteBatch, atlas: TextureAtlas?) {
+            if (atlas == null && __instance !is Hexaghost) {
+                val frozen = __instance.getPower(makeID(Frozen::class.java)) as Frozen
+                if (frozen != null) {
+                    shader.begin()
+                    shader.setUniformf("shadeTimer", frozen.shaderTimer)
+                    sb.shader = shader
+                }
+            }
+        }
+
+
+        @SpireInsertPatch(locator = LocatorImageEnd::class, localvars = ["atlas"])
+        @JvmStatic
+        fun InsertImageEnd(__instance: AbstractMonster, sb: SpriteBatch, atlas: TextureAtlas?) {
+            if (atlas == null && __instance.hasPower(makeID(Frozen::class.java)) && __instance !is Hexaghost) {
+                sb.shader = null
+                shader.end()
+            }
+        }
+
+        @SpireInsertPatch(locator = LocatorSkeletonStart::class)
+        @JvmStatic
+        fun InsertSkeletonStart(__instance: AbstractMonster, sb: SpriteBatch) {
+            val frozen = __instance.getPower(makeID(Frozen::class.java)) as Frozen
+            if (__instance !is Hexaghost) {
+                shader.begin()
+                shader.setUniformf("shadeTimer", frozen.shaderTimer)
+                CardCrawlGame.psb.shader = shader
+            }
+        }
+
+        @SpireInsertPatch(locator = LocatorSkeletonEnd::class)
+        @JvmStatic
+        fun InsertSkeletonEnd(__instance: AbstractMonster, sb: SpriteBatch) {
+            if (__instance.hasPower(makeID(Frozen::class.java)) && __instance !is Hexaghost) {
+                CardCrawlGame.psb.shader = null
+                shader.end()
+            }
+        }
+
+
+        private class LocatorImageStart : SpireInsertLocator() {
+            @Throws(Exception::class)
+            override fun Locate(ctMethodToPatch: CtBehavior): IntArray {
+                val finalMatcher = Matcher.FieldAccessMatcher(AbstractMonster::class.java, "damageFlash")
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher)
+            }
+        }
+
+        private class LocatorImageEnd : SpireInsertLocator() {
+            @Throws(Exception::class)
+            override fun Locate(ctMethodToPatch: CtBehavior): IntArray {
+                val finalMatcher = Matcher.FieldAccessMatcher(AbstractMonster::class.java, "damageFlash")
+                return Arrays.copyOfRange(LineFinder.findAllInOrder(ctMethodToPatch, finalMatcher), 1, 2)
+            }
+        }
+
+        private class LocatorSkeletonStart : SpireInsertLocator() {
+            @Throws(Exception::class)
+            override fun Locate(ctMethodToPatch: CtBehavior): IntArray {
+                val finalMatcher = Matcher.MethodCallMatcher(SkeletonMeshRenderer::class.java, "draw")
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher)
+            }
+        }
+
+        private class LocatorSkeletonEnd : SpireInsertLocator() {
+            @Throws(Exception::class)
+            override fun Locate(ctMethodToPatch: CtBehavior): IntArray {
+                val finalMatcher = Matcher.MethodCallMatcher(PolygonSpriteBatch::class.java, "end")
+                return LineFinder.findInOrder(ctMethodToPatch, finalMatcher)
+            }
         }
     }
 
