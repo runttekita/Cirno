1d0f7f142e96cefb36b2984c88a65fbf0191f497
velvet-halation <buizark@gmail.com>
2019-10-08 01:26:22 -0700

    render zones

--- src/main/kotlin/cirno/characters/SpellZoneManager.kt
+++ src/main/kotlin/cirno/characters/SpellZoneManager.kt
@@ -1,10 +1,14 @@
 package cirno.characters
 
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.evacipated.cardcrawl.modthespire.lib.SpireField
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 
 class SpellZoneManager {
+    var zones = ArrayList<SpellZone>()
 
     @SpirePatch(
             clz = AbstractPlayer::class,
@@ -16,8 +20,27 @@ class SpellZoneManager {
             public var spellZones = SpireField {SpellZoneManager()}
         }
     }
+
+    @SpirePatch(
+            clz = AbstractPlayer::class,
+            method = "renderHand"
+    )
+    public class RenderZones {
+        public companion object {
+            @JvmStatic
+            public fun Prefix(__instance: AbstractPlayer, sb: SpriteBatch) {
+                if (CardCrawlGame.dungeon != null && AbstractDungeon.player != null && AbstractDungeon.player.spellZones.zones.isNotEmpty()) {
+                    for (zone in AbstractDungeon.player.spellZones.zones) {
+                        zone.render(sb)
+                        zone.update()
+                    }
+                }
+            }
+        }
+    }
+
 }
 
-public fun AbstractPlayer.spellZones() : SpellZoneManager {
-    return SpellZoneManager.SpellZones.spellZones.get(this)
-}
+val AbstractPlayer.spellZones: SpellZoneManager
+    get() = SpellZoneManager.SpellZones.spellZones.get(this)
+
