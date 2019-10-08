e64088b5228fb98a2705c7b54c73b7488b6cf9ca
velvet-halation <buizark@gmail.com>
2019-10-08 07:57:23 -0700

    frostyboiy has function

--- src/main/kotlin/cirno/monsters/FrostBoy.kt
+++ src/main/kotlin/cirno/monsters/FrostBoy.kt
@@ -1,21 +1,48 @@
 package cirno.monsters
 
 import cirno.Cirno.Statics.makeID
+import cirno.powers.Cold
+import com.megacrit.cardcrawl.actions.AbstractGameAction
+import com.megacrit.cardcrawl.actions.animations.AnimateSlowAttackAction
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.actions.common.AttackDamageRandomEnemyAction
+import com.megacrit.cardcrawl.actions.common.DamageAction
+import com.megacrit.cardcrawl.cards.DamageInfo
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.monsters.AbstractMonster
 
 class FrostBoy(private val turns: Int) : AbstractMonster(monsterStrings.NAME, ID, turns, AbstractDungeon.player.drawX - 300f, AbstractDungeon.player.drawY, 250f, 250f, "cirno/images/monsters/frostBoy.png") {
 
+    init {
+        damage.add(DamageInfo(this, damageAmt, DamageInfo.DamageType.NORMAL))
+    }
+
     public companion object {
         val ID = makeID(FrostBoy::class.java)
         val monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID)
+        val damageAmt = 12
+    }
+
+    enum class MOVES(public val value: Byte) {
+        ATTACK(0)
     }
 
     override fun getMove(move: Int) {
+        setMove(FrostBoy.MOVES.ATTACK.value, AbstractMonster.Intent.ATTACK_DEBUFF)
     }
 
     override fun takeTurn() {
+        when (nextMove) {
+            0.toByte() -> {
+                val target = AbstractDungeon.getRandomMonster()
+                AbstractDungeon.actionManager.addToBottom(AnimateSlowAttackAction(this))
+                AbstractDungeon.actionManager.addToBottom(DamageAction(target, damage[FrostBoy.MOVES.ATTACK.value.toInt()], AbstractGameAction.AttackEffect.NONE))
+                AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(target, this, Cold(target), 1))
+                AbstractDungeon.actionManager.addToBottom(DamageAction(this, DamageInfo(this, 1, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.NONE))
+            }
+        }
+        rollMove()
     }
 
 }
