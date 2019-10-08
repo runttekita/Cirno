9fbb2b6cde742155f27dd389e2b3ab353f0e2b29
velvet-halation <buizark@gmail.com>
2019-10-07 23:06:16 -0700

    patch

--- src/main/kotlin/cirno/powers/FreezeNextTurn.kt
+++ src/main/kotlin/cirno/powers/FreezeNextTurn.kt
@@ -1,14 +1,20 @@
 package cirno.powers
 
 import cirno.Cirno
+import cirno.Cirno.Statics.makeID
 import cirno.abstracts.CirnoPower
 import cirno.actions.FreezeMonsterAction
+import com.badlogic.gdx.Game
+import com.evacipated.cardcrawl.modthespire.lib.*
+import com.megacrit.cardcrawl.actions.GameActionManager
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction
+import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.core.AbstractCreature
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.localization.PowerStrings
 import com.megacrit.cardcrawl.monsters.AbstractMonster
+import javassist.CtBehavior
 
 class FreezeNextTurn(target: AbstractCreature, turns: Int) : CirnoPower() {
 
@@ -31,8 +37,42 @@ class FreezeNextTurn(target: AbstractCreature, turns: Int) : CirnoPower() {
         amount = turns
     }
 
+    override fun atStartOfTurn() {
+        AbstractDungeon.actionManager.addToBottom(FreezeMonsterAction(owner as AbstractMonster, AbstractDungeon.player))
+        AbstractDungeon.actionManager.addToBottom(ReducePowerAction(owner, owner, this, 1))
+    }
+
     override fun updateDescription() {
         description = DESCRIPTIONS!![0]
+    }
+
+    @SpirePatch(
+            clz = GameActionManager::class,
+            method = "getNextAction"
+    )
+    public class ProcThisAtStartOfPlayerTurn {
+
+        public companion object {
+            @SpireInsertPatch(
+                    locator = Locator::class
+            )
+            fun Insert(__instance: GameActionManager) {
+                for (m in AbstractDungeon.getMonsters().monsters) {
+                    if (m.hasPower(makeID(FreezeNextTurn::class.java))) {
+                        m.getPower(makeID(FreezeNextTurn::class.java)).atStartOfTurn()
+                    }
+                }
+            }
+        }
+
+    }
+
+    public class Locator : SpireInsertLocator() {
+        override fun Locate(ctMethodToPatch: CtBehavior?): IntArray {
+            val matcher = Matcher.MethodCallMatcher(AbstractPlayer::class.java, "applyStartOfTurnPowers")
+            return LineFinder.findInOrder(ctMethodToPatch, matcher)
+        }
+
     }
 
 }
