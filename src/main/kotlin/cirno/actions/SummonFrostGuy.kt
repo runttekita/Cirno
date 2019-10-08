44094347bfca99b37aeb0f5f4dfeaa2030d4f9c1
velvet-halation <buizark@gmail.com>
2019-10-08 07:33:17 -0700

    card

---
+++ src/main/kotlin/cirno/actions/SummonFrostGuy.kt
@@ -0,0 +1,10 @@
+package cirno.actions
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction
+
+class SummonFrostGuy(private val upgraded: Boolean) : AbstractGameAction() {
+
+    override fun update() {
+    }
+
+}
