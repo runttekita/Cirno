869ca61c21c17b01ae236637c9221911f0a470ea
velvet-halation <buizark@gmail.com>
2019-10-09 07:24:04 -0700

    frozen discard card

---
+++ src/main/kotlin/cirno/cards/FrozenDiscard.kt
@@ -0,0 +1,50 @@
+package cirno.cards
+
+import cirno.Cirno.Statics.makeID
+import cirno.abstracts.CirnoCard
+import cirno.powers.FrozenDiscardP
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class FrozenDiscard : CirnoCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MAGIC_UP, COST) {
+
+    init {
+        baseDamage = DAMAGE
+        baseBlock = BLOCK
+        magicNumber = MAGIC
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster?) {
+        act(ApplyPowerAction(p, p, FrozenDiscardP()))
+    }
+
+    override fun upgrade() {
+        if (!upgraded) {
+            upgradeName()
+            isInnate = true
+            rawDescription = cardStrings.UPGRADE_DESCRIPTION
+            initializeDescription()
+        }
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(FrozenDiscard::class.java.simpleName))
+        private val COST = 1
+        private val TYPE = CardType.POWER
+        private val RARITY = AbstractCard.CardRarity.UNCOMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 0
+        private val MAGIC_UP = 0
+        private val DAMAGE = 0
+        private val BLOCK = 0
+        private val MAGIC = 0
+    }
+}
