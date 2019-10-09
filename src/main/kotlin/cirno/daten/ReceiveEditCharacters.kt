38bd841157da5b8614a6ec44e78fffd6fcdb3ba8
velvet-halation <buizark@gmail.com>
2019-10-09 00:55:41 -0700

    here too

--- src/main/kotlin/cirno/daten/ReceiveEditCharacters.kt
+++ src/main/kotlin/cirno/daten/ReceiveEditCharacters.kt
@@ -2,13 +2,15 @@ package cirno.daten
 
 import cirno.characters.CirnoChar
 import basemod.BaseMod
+import cirno.Cirno.Statics.makeID
+import com.megacrit.cardcrawl.core.CardCrawlGame
 
 class ReceiveEditCharacters() {
     val button = "cirno/images/charSelect/DefaultCharacterButton.png"
     val portrait = "cirno/images/charSelect/DefaultCharacterPortraitBG.png"
     init {
-
-        BaseMod.addCharacter(CirnoChar.Enums.enums.Cirno?.let { CirnoChar("Cirno", it) },
+        val charStrings = CardCrawlGame.languagePack.getCharacterString(makeID(CirnoChar::class.java))
+        BaseMod.addCharacter(CirnoChar.Enums.enums.Cirno?.let { CirnoChar(charStrings.NAMES[0], it) },
                 button, portrait, CirnoChar.Enums.enums.Cirno)
     }
 
