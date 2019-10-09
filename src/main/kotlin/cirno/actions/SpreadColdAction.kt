505e9bde1892feac096eafbfb09f06a91efe18a0
velvet-halation <buizark@gmail.com>
2019-10-09 00:16:19 -0700

    Revert "dont spread cold to frostboy"
    This reverts commit f5b166aa8ca228c33511e45967161e7046a75d53.

--- src/main/kotlin/cirno/actions/SpreadColdAction.kt
+++ src/main/kotlin/cirno/actions/SpreadColdAction.kt
@@ -1,7 +1,6 @@
 package cirno.actions
 
 import cirno.Cirno.Statics.makeID
-import cirno.monsters.FrostBoy
 import cirno.powers.Cold
 import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
@@ -18,13 +17,10 @@ class SpreadColdAction(private val targeted: AbstractCreature) : AbstractGameAct
         if (targeted.hasPower(makeID(Cold::class.java)) && target is AbstractMonster) {
             val slot = AbstractDungeon.getMonsters().monsters.indexOf(target as AbstractMonster)
             var left: AbstractMonster? = null
-            if (slot > 0 && AbstractDungeon.getMonsters().monsters.get(slot - 1) !is FrostBoy) {
-                left = AbstractDungeon.getMonsters().monsters[slot - 1]
-            }
+            if (slot > 0) left = AbstractDungeon.getMonsters().monsters[slot - 1]
             var right: AbstractMonster? = null
-            if (slot < AbstractDungeon.getMonsters().monsters.size - 1 && AbstractDungeon.getMonsters().monsters.get(slot + 1) !is FrostBoy) {
+            if (slot < AbstractDungeon.getMonsters().monsters.size - 1)
                 right = AbstractDungeon.getMonsters().monsters[slot + 1]
-            }
             if (left != null && !left.isDeadOrEscaped) {
                 AbstractDungeon.actionManager.addToBottom(ApplyPowerAction(left, AbstractDungeon.player, Cold(left)))
             }
