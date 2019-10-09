cd32b19970757da18634d86f9b6791a2d647fd45
velvet-halation <buizark@gmail.com>
2019-10-08 22:38:53 -0700

    change this

--- src/main/kotlin/cirno/actions/SummonFrostGuy.kt
+++ src/main/kotlin/cirno/actions/SummonFrostGuy.kt
@@ -1,23 +1,23 @@
 package cirno.actions
 
 import cirno.monsters.FrostBoy
+import cirno.monsters.frostKing
 import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 
 class SummonFrostGuy(private val turns: Int) : AbstractGameAction() {
 
     override fun update() {
-        for (monster in AbstractDungeon.getMonsters().monsters) {
-            if (monster.id == FrostBoy.ID) {
-                if (monster.currentHealth + turns > monster.maxHealth) {
-                    monster.maxHealth += turns
-                }
-                monster.heal(turns)
-                isDone = true
-                return
+        val monster = AbstractDungeon.player.frostKing
+        if (monster != null) {
+            if (monster.currentHealth + turns > monster.maxHealth) {
+                monster.maxHealth += turns
             }
+            monster.heal(turns)
+            isDone = true
+            return
         }
-        AbstractDungeon.getMonsters().addMonster(AbstractDungeon.getMonsters().monsters.size, FrostBoy(turns))
+        AbstractDungeon.player.frostKing = FrostBoy(turns)
         isDone = true
     }
 
