05df1effbd625ec1142c8308965bd04173798168
velvet-halation <buizark@gmail.com>
2019-10-09 05:38:04 -0700

    freeze spell

---
+++ src/main/kotlin/cirno/cards/FreezeSpell.kt
@@ -0,0 +1,39 @@
+package cirno.cards
+
+import cirno.Cirno.Statics.makeID
+import cirno.abstracts.CirnoCard
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.localization.CardStrings
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+
+
+class FreezeSpell : CirnoCard(cardStrings, COST, TYPE, RARITY, TARGET, DAMAGE_UP, BLOCK_UP, MAGIC_UP, 1) {
+
+    init {
+        baseDamage = DAMAGE
+        baseBlock = BLOCK
+        magicNumber = MAGIC
+        baseMagicNumber = magicNumber
+    }
+
+    override fun use(p: AbstractPlayer, m: AbstractMonster?) {
+
+    }
+
+    companion object {
+        private val cardStrings = CardCrawlGame.languagePack.getCardStrings(
+                makeID(FreezeSpell::class.java.simpleName))
+        private val COST = 2
+        private val TYPE = CardType.SKILL
+        private val RARITY = AbstractCard.CardRarity.UNCOMMON
+        private val TARGET = AbstractCard.CardTarget.SELF
+        private val DAMAGE_UP = 0
+        private val BLOCK_UP = 0
+        private val MAGIC_UP = 0
+        private val DAMAGE = 0
+        private val BLOCK = 0
+        private val MAGIC = 0
+    }
+}
