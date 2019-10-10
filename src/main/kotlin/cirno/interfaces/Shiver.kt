421045e50a005359cc2e16bb60be8643906e6aed
velvet-halation <buizark@gmail.com>
2019-10-10 05:06:22 -0700

    shiver patches

--- src/main/kotlin/cirno/interfaces/Shiver.kt
+++ src/main/kotlin/cirno/interfaces/Shiver.kt
@@ -1,8 +1,11 @@
 package cirno.interfaces
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireField
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
+import com.evacipated.cardcrawl.modthespire.lib.*
+import com.megacrit.cardcrawl.actions.GameActionManager
 import com.megacrit.cardcrawl.characters.AbstractPlayer
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import javassist.CtBehavior
+import com.megacrit.cardcrawl.actions.AbstractGameAction
 
 interface Shiver {
 
@@ -14,6 +17,47 @@ interface Shiver {
         public companion object {
             @JvmField
             public var isShivering =  SpireField<Boolean> {false}
+        }
+    }
+
+    @SpirePatch(
+            clz = GameActionManager::class,
+            method = "getNextAction"
+    )
+    public class SetShivering {
+        public companion object {
+            @SpireInsertPatch(
+                    locator = Locator::class
+            )
+            @JvmStatic
+            fun Insert(__instance: GameActionManager) {
+                AbstractDungeon.actionManager.addToBottom(object : AbstractGameAction() {
+                    override fun update() {
+                        AbstractDungeon.player.isShivering = true
+                        isDone = true
+                    }
+
+                })
+            }
+        }
+    }
+
+    public class Locator : SpireInsertLocator() {
+        override fun Locate(p0: CtBehavior?): IntArray {
+            val matcher = Matcher.MethodCallMatcher(AbstractPlayer::class.java, "applyStartOfTurnPostDrawRelics")
+            return LineFinder.findInOrder(p0, matcher)
+        }
+
+    }
+
+    @SpirePatch(
+            clz = GameActionManager::class,
+            method = "endTurn"
+    )
+    public companion object {
+        @JvmStatic
+        fun Prefix(__instance: GameActionManager) {
+            AbstractDungeon.player.isShivering = false
         }
     }
 
