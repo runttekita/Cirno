57e490706d3c2af68763e74d6c3cac88413d6901
velvet-halation <buizark@gmail.com>
2019-10-11 02:26:06 -0700

    snowflake strings

--- make.sh
+++ make.sh
@@ -101,6 +101,7 @@ then
   STUDIOUS_EFFORTS='Upgrade the first !M! cards you draw outside of the start of the turn'
   SNOWMAN='If you drew !D! Attacks this way, ALL attacks gain !B! damage.'
   KISS_OF_DEATH='This enemy takes !M! damage for every card drawn this turn.'
+  SNOWFLAKE='Reduce the cost of ALL cards in your hand to 0 this combat.'
 fi
 
 # Copy into production folder
@@ -115,6 +116,7 @@ cp ${DEV_STRINGS}character.json ${PROD_STRINGS}character.json
 
 # Replace strings
 PROD_JSON=${PROD_STRINGS}card.json
+sed -i s/\$snowflake/"${SNOWFLAKE}"/g ${PROD_JSON}
 sed -i s/\$kissofdeath/"${KISS_OF_DEATH}"/g ${PROD_JSON}
 sed -i s/\$snowman/"${SNOWMAN}"/g ${PROD_JSON}
 sed -i s/\$studiousefforts/"${STUDIOUS_EFFORTS}"/g ${PROD_JSON}
